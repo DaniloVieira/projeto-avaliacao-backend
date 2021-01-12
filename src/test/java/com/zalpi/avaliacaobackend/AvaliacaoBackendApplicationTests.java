d409e17546e5b7508ab4872230d232b657fdc74e
Danilo <methodic4u@gmail.com>
2021-01-12 13:02:19 -0300

    first commit

---
+++ src/test/java/com/zalpi/avaliacaobackend/AvaliacaoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zalpi.avaliacaobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaliacaoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
