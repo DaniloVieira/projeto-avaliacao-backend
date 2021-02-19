b16de7bccb760aee7a58daba9f39a2ba48b6826e
Danilo <methodic4u@gmail.com>
2021-02-19 19:11:53 -0300

    recatory on list activities to use query params

--- src/main/java/com/zalpi/avaliacaobackend/rest/endpoint/ActivityEndPoint.java
+++ src/main/java/com/zalpi/avaliacaobackend/rest/endpoint/ActivityEndPoint.java
@@ -1,9 +1,10 @@
 package com.zalpi.avaliacaobackend.rest.endpoint;
 
+import java.time.LocalDateTime;
+
 import com.zalpi.avaliacaobackend.dto.ActivityDTO;
 import com.zalpi.avaliacaobackend.dto.filter.ActivityFilterDTO;
 import com.zalpi.avaliacaobackend.dto.response.ResponseObject;
-import com.zalpi.avaliacaobackend.model.Activity;
 import com.zalpi.avaliacaobackend.service.ActivityService;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import static com.zalpi.avaliacaobackend.util.misc.ResponseUtil.createResponse;
@@ -27,12 +29,31 @@ public class ActivityEndPoint {
 	private ActivityService activityService;
 
 	@PostMapping("save")
-	public ResponseEntity<ResponseObject> saveActivity(@RequestBody ActivityDTO activity){
+	public ResponseEntity<ResponseObject> saveActivity(@RequestBody ActivityDTO activity) {
 		return createResponse(activityService.saveActivity(activity));
 	}
 
 	@GetMapping("list")
-	public ResponseEntity<ResponseObject> listByFilters(@RequestBody ActivityFilterDTO filter){
+	public ResponseEntity<ResponseObject> listByFilters(
+		@RequestParam(required = false) Long projectId,
+		@RequestParam(required = false) Long contributorId,
+		@RequestParam(required = false) String description,
+		@RequestParam(required = false) LocalDateTime dtInitialStart,
+		@RequestParam(required = false) LocalDateTime dtInitialEnd,
+		@RequestParam(required = false) LocalDateTime dtFinalStart,
+		@RequestParam(required = false) LocalDateTime dtFinalEnd,
+		@RequestParam Integer page,
+		@RequestParam Integer pageSize) {
+		ActivityFilterDTO filter = new ActivityFilterDTO();
+		filter.setProjectId(projectId);
+		filter.setContributorId(contributorId);
+		filter.setDescription(description);
+		filter.setDtInitialStart(dtInitialStart);
+		filter.setDtInitialEnd(dtInitialEnd);
+		filter.setDtFinalStart(dtFinalStart);
+		filter.setDtFinalEnd(dtFinalEnd);
+		filter.setPage(page);
+		filter.setPageSize(pageSize);
 		return createResponse(activityService.listByFilters(filter));
 	}
 
